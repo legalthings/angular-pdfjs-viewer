6c45383fd29272680ed92ff95dacc26c6e4d9d40
moesjarraf <moesjarraf@gmail.com>
2015-08-21 02:43:24 +0200

    Temporary resetting source

--- dist/web/viewer.js
+++ dist/web/viewer.js
@@ -27,13 +27,7 @@
 
 'use strict';
 
-
-
-var SCRIPTS = document.getElementsByTagName('script');
-var PATH = SCRIPTS[SCRIPTS.length-1].src;
-var FILENAME = getFileName(PATH);
-var FOLDER = getLocation(PATH).pathname.replace(FILENAME, '');
-
+var DEFAULT_URL = 'compressed.tracemonkey-pldi-09.pdf';
 var DEFAULT_SCALE_DELTA = 1.1;
 var MIN_SCALE = 0.25;
 var MAX_SCALE = 10.0;
@@ -42,11 +36,11 @@ var SCALE_SELECT_CONTAINER_PADDING = 8;
 var SCALE_SELECT_PADDING = 22;
 var PAGE_NUMBER_LOADING_INDICATOR = 'visiblePageIsLoading';
 var DISABLE_AUTO_FETCH_LOADING_BAR_TIMEOUT = 5000;
-PDFJS.imageResourcesPath = FOLDER + 'images/';
-PDFJS.workerSrc = FOLDER + '../build/pdf.worker.js';
-PDFJS.cMapUrl = FOLDER + 'cmaps/';
-PDFJS.cMapPacked = true;
-PDFJS.DEFAULT_URL = FOLDER + '../demo/example.pdf';
+
+PDFJS.imageResourcesPath = './images/';
+  PDFJS.workerSrc = '../build/pdf.worker.js';
+  PDFJS.cMapUrl = '../web/cmaps/';
+  PDFJS.cMapPacked = true;
 
 var mozL10n = document.mozL10n || document.webL10n;
 
@@ -117,15 +111,6 @@ function getFileName(url) {
     query > 0 ? query : url.length);
   return url.substring(url.lastIndexOf('/', end) + 1, end);
 }
-
-function getLocation (href) {
-    var location = document.createElement("a");
-    location.href = href;
-
-    if (location.host == '') location.href = location.href;
-
-    return location;
-};
 
 /**
  * Returns scale factor for the canvas. It makes sense for the HiDPI displays.
@@ -6059,6 +6044,7 @@ var PDFViewerApplication = {
 
     var pdfLinkService = new PDFLinkService();
     this.pdfLinkService = pdfLinkService;
+
     var container = document.getElementById('viewerContainer');
     var viewer = document.getElementById('viewer');
     this.pdfViewer = new PDFViewer({
@@ -7006,7 +6992,8 @@ function webViewerLoad(evt) {
 function webViewerInitialized() {
   var queryString = document.location.search.substring(1);
   var params = parseQueryString(queryString);
-  var file = PDFJS.DEFAULT_URL;
+  var file = 'file' in params ? params.file : DEFAULT_URL;
+
   var fileInput = document.createElement('input');
   fileInput.id = 'fileInput';
   fileInput.className = 'fileInput';
@@ -7223,12 +7210,7 @@ function webViewerInitialized() {
   }
 }
 
-// document.addEventListener('DOMContentLoaded', webViewerLoad, true);
-PDFJS.webViewerLoad = function (src) {
-  if (src) PDFJS.DEFAULT_URL = src;
-
-  webViewerLoad();
-}
+document.addEventListener('DOMContentLoaded', webViewerLoad, true);
 
 document.addEventListener('pagerendered', function (e) {
   var pageNumber = e.detail.pageNumber;
